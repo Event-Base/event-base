4cd976dc0f4d7f896b9be664c7fb578f696c217b
vyshnav <vyshnavravi599@gmail.com>
2024-06-28 17:45:27 +0530

    feat: Update PrismaClient import and add Neon database adapter

--- app/src/lib/db.ts
+++ app/src/lib/db.ts
@@ -1,15 +1,18 @@
-import { PrismaClient } from '@prisma/client'
-
+import { PrismaClient } from "@prisma/client";
+import { Pool } from "@neondatabase/serverless";
+import { PrismaNeon } from "@prisma/adapter-neon";
 const prismaClientSingleton = () => {
-  return new PrismaClient()
-}
+    // const neon = new Pool({ connectionString: process.env.DATABASE_URL });
+    // const adapter = new PrismaNeon(neon);
+    return new PrismaClient();
+};
 
 declare const globalThis: {
-  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+    prismaGlobal: ReturnType<typeof prismaClientSingleton>;
 } & typeof global;
 
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
 
-export default prisma
+export default prisma;
 
-if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
+if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
