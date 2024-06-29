afee371ecee524429bfd4f1d827e68b32bbf300d
vyshnav <vyshnavravi599@gmail.com>
2024-06-29 13:06:02 +0530

    feat: Update NextAuth configuration for role

--- app/src/auth.ts
+++ app/src/auth.ts
@@ -1,7 +1,23 @@
-import NextAuth from "next-auth";
+import NextAuth, { DefaultSession } from "next-auth";
 import prisma from "@/lib/db";
 import Google from "next-auth/providers/google";
 import { PrismaAdapter } from "@auth/prisma-adapter";
+import { UserRole } from "@prisma/client";
+import {JWT} from "next-auth/jwt"
+
+declare module "next-auth" {
+    interface Session {
+      user: {
+        role: UserRole;
+      } & DefaultSession["user"];
+    }
+  }
+  
+  declare module "next-auth/jwt" {
+    interface JWT {
+      role: UserRole;
+    }
+  }
 export const { handlers, signIn, signOut, auth } = NextAuth({
     adapter: PrismaAdapter(prisma),
     providers: [Google],
