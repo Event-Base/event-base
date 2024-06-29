730a50bc8faf58080a5becc51ead7990777680f0
vyshnav <vyshnavravi599@gmail.com>
2024-06-29 15:43:10 +0530

    feat: Add admin page with authentication check

---
+++ app/src/app/admin/page.tsx
@@ -0,0 +1,14 @@
+import getSession from "@/lib/getSession";
+import { UserRole } from "@prisma/client";
+import { notFound } from "next/navigation";
+export default async function Admin() {
+    const session = await getSession();
+    if(!session || session.user.role !== UserRole.ADMIN){
+        return notFound()
+    }
+    return (
+        <>
+            <div className="min-h-screen text-2xl flex items-center justify-center">This is admin page</div>
+        </>
+    );
+}
