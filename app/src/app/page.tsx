db1595073f0a3f08742bd84162fbf4d420ca1113
vyshnav <vyshnavravi599@gmail.com>
2024-06-29 13:06:44 +0530

    role

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,12 +1,23 @@
 import { auth } from "@/auth";
+import Link from "next/link";
 
-export default async  function Home() {
-  const session = await auth();
-  const user = session?.user
-    return(
-      <div>
-        <p >{user ? user.name : "No user"}</p>
-      </div>
+
+export default async function Home() {
+    const session = await auth();
+    const user = session?.user;
+    
+    return (
+        <div className="min-h-screen text-2xl flex items-center justify-center">
+            {user ? (
+                `Welcome ${user.name} your role is ${user.role}`
+            ) : (
+                <Link
+                    href={"/api/auth/signin?callbackUrl=/profile"}
+                    className="w-24 h-10 bg-blue-600 rounded-md text-center"
+                >
+                    Login
+                </Link>
+            )}
+        </div>
     );
-        
 }
