d5421c4adc660e60cd608973447a56bb771a6b90
vyshnav <vyshnavravi599@gmail.com>
2024-06-30 15:57:48 +0530

    refactor updateUserRole function to improve readability and maintainability

--- app/src/app/actions/index.ts
+++ app/src/app/actions/index.ts
@@ -3,22 +3,27 @@ import prisma from "@/lib/db";
 import { UserRole } from "@prisma/client";
 
 export default async function updateUserRole(id: string) {
-    const user = await prisma.user.findUnique({
-        where: {
-            id,
-        },
-    });
+    try {
+        const user = await prisma.user.findUnique({
+            where: { id },
+        });
 
-    if (!user) return;
+        if (!user) {
+            console.error("User not found");
+            return false;
+        }
 
-    const role = user.role === UserRole.ADMIN ? UserRole.PARTICIPANT : UserRole.ADMIN;
+    
+        const newRole = user.role === UserRole.ADMIN ? UserRole.PARTICIPANT : UserRole.ADMIN;
 
-    await prisma.user.update({
-        where: {
-            id,
-        },
-        data: {
-            role,
-        },
-    });
+        await prisma.user.update({
+            where: { id },
+            data: { role: newRole },
+        });
+
+        return true;
+    } catch (error) {
+        console.error("Error updating user role:", error);
+        return false;
+    }
 }
