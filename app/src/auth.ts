6d2110f93da94ce6f2ee610f9b9cd2b87c830673
vyshnav <vyshnavravi599@gmail.com>
2024-07-03 12:48:21 +0530

    feat: Update Google provider in auth.ts to include profile mapping

--- app/src/auth.ts
+++ app/src/auth.ts
@@ -1,24 +1,43 @@
 import NextAuth, { DefaultSession } from "next-auth";
 import prisma from "@/lib/db";
-import Google from "next-auth/providers/google";
+import Google, { GoogleProfile } from "next-auth/providers/google";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { UserRole } from "@prisma/client";
-import {JWT} from "next-auth/jwt"
+import { JWT } from "next-auth/jwt";
 
 declare module "next-auth" {
     interface Session {
-      user: {
-        role: UserRole;
-      } & DefaultSession["user"];
+        user: {
+            role: UserRole;
+        } & DefaultSession["user"];
     }
-  }
-  
-  declare module "next-auth/jwt" {
+}
+
+declare module "next-auth/jwt" {
     interface JWT {
-      role: UserRole;
+        role: UserRole;
     }
-  }
+}
 export const { handlers, signIn, signOut, auth } = NextAuth({
     adapter: PrismaAdapter(prisma),
-    providers: [Google],
+    providers: [
+        Google({
+            profile(profile: GoogleProfile) {
+                return {
+                    id: profile.sub,
+                    name: profile.name,
+                    email: profile.email,
+                    image: profile.picture,
+                    role: (profile.role as UserRole) ?? UserRole.PARTICIPANT,
+                };
+            },
+        }),
+    ],
+    callbacks: {
+        async session({ session, token }) {
+            return {
+                ...session,
+            };
+        },
+    },
 });
