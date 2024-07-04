16abe3c75888068d5cdb9b6a59620c871ba91022
vyshnav <vyshnavravi599@gmail.com>
2024-07-04 18:32:37 +0530

    update : remove check for auth because added a middleware for that

--- app/src/app/admin/users/page.tsx
+++ app/src/app/admin/users/page.tsx
@@ -5,16 +5,14 @@ import AdminNavBar from "@/components/admin/AdminNavBar";
 import UsersList from "@/components/admin/UsersList";
 
 export default async function Admin() {
-  const session = await getSession();
-  if (!session || session.user.role !== UserRole.ADMIN) {
-    return notFound();
-  }
-  return (
-    <>
-      <div className="flex min-h-screen w-full flex-col bg-background">
-        <AdminNavBar session={session} />
-        <UsersList />
-      </div>
-    </>
-  );
+    const session = await getSession();
+
+    return (
+        <>
+            <div className="flex min-h-screen w-full flex-col bg-background">
+                <AdminNavBar session={session} />
+                <UsersList />
+            </div>
+        </>
+    );
 }
