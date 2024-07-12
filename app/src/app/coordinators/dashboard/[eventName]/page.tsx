066c1442cb854da5a0cc9fbff504707d8276e599
vyshnav <vyshnavravi599@gmail.com>
2024-07-12 17:56:21 +0530

    refactor: Add lazy loading for Dashboard component in [eventName] page

--- app/src/app/coordinators/dashboard/[eventName]/page.tsx
+++ app/src/app/coordinators/dashboard/[eventName]/page.tsx
@@ -1,13 +1,15 @@
 import Dashboard from "@/components/dashboard/Dashboad";
 import getSession from "@/lib/getSession";
 import { Suspense } from "react";
+import { SkeletonCard } from "@/components/skeletonCard/SkeletonCard";
 const page = async () => {
     const session = await getSession();
 
     return (
         <div>
-            <Suspense fallback={<div>Loading</div>}>
+            <Suspense fallback={<SkeletonCard />}>
                 <Dashboard session={session} />
+                
             </Suspense>
         </div>
     );
