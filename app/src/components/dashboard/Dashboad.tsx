844ed42d7a2ff89faf064dce06be65053a165872
vyshnav <vyshnavravi599@gmail.com>
2024-07-13 07:34:36 +0530

    refactor: Update Dashboard component to display remaining days

--- app/src/components/dashboard/Dashboad.tsx
+++ app/src/components/dashboard/Dashboad.tsx
@@ -10,12 +10,13 @@ import {
     Package2,
     Search,
     Users,
+    X,
 } from "lucide-react";
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
-import { usePathname } from "next/navigation";
+import { notFound, usePathname } from "next/navigation";
 
 import {
     DropdownMenu,
@@ -31,6 +32,7 @@ import { CartesianGrid, Line, LineChart, XAxis } from "recharts";
 import { getIndividualEventDetailsProp } from "@/types";
 import { useQuery } from "@tanstack/react-query";
 import { getIndividualEventDetails } from "@/app/actions";
+import { getRemainingDay } from "@/lib/helper";
 
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { ChartConfig, ChartContainer, ChartTooltip, ChartTooltipContent } from "@/components/ui/chart";
@@ -90,6 +92,10 @@ export default function Dashboard({ session }: any) {
             color: "hsl(var(--chart-1))",
         },
     } satisfies ChartConfig;
+
+    const remainingDay = getRemainingDay(data?.date.toDateString() as string );
+    
+    
 
     return (
         <div className="flex min-h-screen w-full flex-col">
@@ -213,11 +219,11 @@ export default function Dashboard({ session }: any) {
                     </Card>
                     <Card x-chunk="A card showing the total active users and the percentage difference from last hour.">
                         <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                            <CardTitle className="text-sm font-medium">Coordinator</CardTitle>
+                            <CardTitle className="text-sm font-medium">Day remaining</CardTitle>
                             <Activity className="h-4 w-4 text-muted-foreground" />
                         </CardHeader>
                         <CardContent>
-                            <div className="text-sm font-bold">{data?.coordinatorEmail ?? ""}</div>
+                            <div className="text-2xl font-bold">{isNaN(remainingDay) ? 0 : remainingDay}</div>
                             <p className="text-xs text-muted-foreground"></p>
                         </CardContent>
                     </Card>
