4b716f9cf6d21ded03207e9e940b359f606ce646
vyshnav <vyshnavravi599@gmail.com>
2024-07-13 07:49:43 +0530

    refactor: Fix getRemainingDay function to handle negative difference days

--- app/src/lib/helper.ts
+++ app/src/lib/helper.ts
@@ -4,6 +4,9 @@ export const getRemainingDay = (targetDateStr: string) => {
     const currentDate = new Date();
     const differenceMs: number = targetDate.getTime() - currentDate.getTime();
     differenceDays = Math.ceil(differenceMs / (1000 * 60 * 60 * 24));
+    if(differenceDays < 0 ){
+        differenceDays = 0
+    }
 
     return differenceDays;
 };
