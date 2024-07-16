c32a2f4f1b515076e89c3b9daaa2599c1c6db53e
vyshnav <vyshnavravi599@gmail.com>
2024-07-16 22:14:08 +0530

    feat: Add ThemeToggle component and update npm dependencies

--- app/src/components/layouts/Providers.tsx
+++ app/src/components/layouts/Providers.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { SessionProvider } from "next-auth/react";
 import { TailwindIndicator } from "@/components/shared/tailwind";
+import ThemeProvider from "./ThemeToggle/theme-provider";
 
 const queryClient = new QueryClient();
 export default function Providers({ children }: { children: React.ReactNode }) {
@@ -12,8 +13,10 @@ export default function Providers({ children }: { children: React.ReactNode }) {
         <>
             <QueryClientProvider client={queryClient}>
                 <SessionProvider>
+                <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
                     {children}
                     <ReactQueryDevtools />
+                    </ThemeProvider>
                     <TailwindIndicator />
                 </SessionProvider>
             </QueryClientProvider>
