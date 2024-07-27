ae2a0f548773d37b8106b209dfd99cde04bc6c93
vyshnav <vyshnavravi599@gmail.com>
2024-07-27 09:10:15 +0530

    refactor: Add ErrorFallback component to handle errors

---
+++ app/src/components/errorFallback/ErrorFallback.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ErrorFallback = () => {
+  return (
+    <div className='h-screen w-screen flex justify-center items-center dark:bg-white bg-black text-7xl text-red-600'>
+      Opps something went wrong
+    </div>
+  )
+}
+
+export default ErrorFallback
