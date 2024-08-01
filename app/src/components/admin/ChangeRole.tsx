a1ca894d84fab223231e6c809d246fb4ca9c7766
vyshnav <vyshnavravi599@gmail.com>
2024-08-01 22:40:49 +0530

    feat: Improve ChangeRole component UI and functionality

--- app/src/components/admin/ChangeRole.tsx
+++ app/src/components/admin/ChangeRole.tsx
@@ -2,39 +2,33 @@
 import { makeAdmin, makeParticipant } from "@/app/actions";
 import { Button } from "@/components/ui/button";
 
-export default function ChangeRole({
-  userId,
-  userRole,
-}: {
-  userId: any;
-  userRole: string;
-}) {
-  async function handleMakeAdmin() {
-    await makeAdmin(userId);
-  }
+export default function ChangeRole({ userId, userRole }: { userId: any; userRole: string }) {
+    async function handleMakeAdmin() {
+        await makeAdmin(userId);
+    }
 
-  async function handleMakeParticipant() {
-    await makeParticipant(userId);
-  }
+    async function handleMakeParticipant() {
+        await makeParticipant(userId);
+    }
 
-  return (
-    <div className="">
-      <Button
-        className={`bg-foreground hover:bg-muted-foreground font-bold py-2 px-4 rounded ${
-          userRole === "ADMIN" ? "hidden" : ""
-        }`}
-        onClick={handleMakeAdmin}
-      >
-        Make Admin
-      </Button>
-      <Button
-        className={`bg-foreground hover:bg-muted-foreground font-bold py-2 px-4 rounded ${
-          userRole === "PARTICIPANT" ? "hidden" : ""
-        }`}
-        onClick={handleMakeParticipant}
-      >
-        Make Participant
-      </Button>
-    </div>
-  );
+    return (
+        <div className="">
+            <Button
+                className={`bg-foreground hover:bg-muted-foreground font-bold py-2 px-4 rounded dark:bg-black ${
+                    userRole === "ADMIN" ? "hidden" : ""
+                }`}
+                onClick={handleMakeAdmin}
+            >
+                Make Admin
+            </Button>
+            <Button
+                className={`bg-foreground hover:bg-muted-foreground font-bold py-2 px-4 rounded dark:bg-black ${
+                    userRole === "PARTICIPANT" ? "hidden" : ""
+                }`}
+                onClick={handleMakeParticipant}
+            >
+                Make Participant
+            </Button>
+        </div>
+    );
 }
