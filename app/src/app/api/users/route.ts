a4f5ff24fab463f72749e02e3709f9fcd1b7ccf3
vyshnav <vyshnavravi599@gmail.com>
2024-08-03 12:15:23 +0530

    feat: Add search functionality to user API route

--- app/src/app/api/users/route.ts
+++ app/src/app/api/users/route.ts
@@ -4,11 +4,19 @@ import { NextResponse } from "next/server";
 export async function GET(req: Request) {
     const { searchParams } = new URL(req.url);
     const page = parseInt(searchParams.get('page') || '1');
+    const search = searchParams.get('search') || ""
     const limit = 10;
-    
+     
     const users = await prisma.user.findMany({
         skip: (page - 1) * limit,
         take: limit,
+        where: {
+          name:{
+            contains: search,
+            mode: 'insensitive'
+
+          }
+        }
       });
 
       return NextResponse.json({ users });
