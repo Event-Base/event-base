3841923783c622daa14d2290844c352c7dadb639
vyshnav <vyshnavravi599@gmail.com>
2024-08-03 22:36:47 +0530

    chore: Refactor report page form for better code organization and user experience

--- app/src/app/report/page.tsx
+++ app/src/app/report/page.tsx
@@ -1,15 +1,56 @@
 "use client";
 import React, { useState, useEffect } from "react";
 import { useSession } from "next-auth/react";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { toast } from "@/components/ui/use-toast";
+import { createIsuue } from "../actions";
 
 const Signup: React.FC = () => {
-    const [email, setEmail] = useState("");
-    const [name, setName] = useState("");
-    const [description, setDescription] = useState("");
-    const [issueName, setIssueName] = useState("");
-    const [isDarkMode, setIsDarkMode] = useState(false);
-
     const { data: session } = useSession();
+    const [formData, setFormData] = useState({
+        email: session?.user.email ?? "",
+        description: "",
+        issueName: "",
+    });
+    const [isDarkMode, setIsDarkMode] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = event.target;
+        setFormData((prevData) => ({
+            ...prevData,
+            [name]: value,
+        }));
+    };
+
+    const queryClient = useQueryClient();
+
+    const mutation = useMutation({
+        mutationFn: async () => {
+            setLoading(true);
+            return await createIsuue(formData);
+        },
+        onSuccess: () => {
+            setLoading(false);
+            toast({
+                description: "Issue reported successfully",
+                title: "Success",
+            });
+            void queryClient.invalidateQueries({ queryKey: ["issues"] });
+        },
+        onError: (error) => {
+            setLoading(false);
+            toast({
+                description: error.message,
+                title: "Error",
+                variant: "destructive",
+            });
+        },
+    });
+
+    const handleSubmit = (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+        mutation.mutate();
+    };
 
     useEffect(() => {
         if (isDarkMode) {
@@ -19,12 +60,6 @@ const Signup: React.FC = () => {
         }
     }, [isDarkMode]);
 
-    const handleSubmit = (e: React.FormEvent) => {
-        e.preventDefault();
-        setEmail(session?.user.email || "");
-        setName(session?.user.name || "");
-    };
-
     return (
         <div className="min-h-screen flex items-center justify-center bg-gray-100 dark:bg-gray-900">
             <div className="absolute top-4 right-4"></div>
@@ -32,20 +67,7 @@ const Signup: React.FC = () => {
                 <h2 className="text-2xl font-bold mb-6 text-center text-gray-900 dark:text-white">
                     Report a issue
                 </h2>
-                <form onSubmit={handleSubmit}>
-                    <div className="mb-4">
-                        <label htmlFor="email" className="block text-gray-700 dark:text-gray-300 mb-2">
-                            Name
-                        </label>
-                        <input
-                            type="name"
-                            id="name"
-                            value={session?.user.name || ""}
-                            className="w-full p-2 border border-gray-300 dark:border-gray-700 rounded-lg bg-white dark:bg-gray-700 text-gray-900 dark:text-gray-200"
-                            required
-                            disabled={true}
-                        />
-                    </div>
+                <form>
                     <div className="mb-4">
                         <label htmlFor="email" className="block text-gray-700 dark:text-gray-300 mb-2">
                             Email
@@ -64,11 +86,11 @@ const Signup: React.FC = () => {
                             Name for issue
                         </label>
                         <input
-                            type="nameforissue"
+                            name="issueName"
                             id="nameforissue"
-                            value={issueName}
+                            value={formData.issueName}
                             onChange={(e) => {
-                                setIssueName(e.target.value);
+                                handleChange(e);
                             }}
                             className="w-full p-2 border border-gray-300 dark:border-gray-700 rounded-lg bg-white dark:bg-gray-700 text-gray-900 dark:text-gray-200"
                             required
@@ -78,11 +100,12 @@ const Signup: React.FC = () => {
                         <label htmlFor="email" className="block text-gray-700 dark:text-gray-300 mb-2">
                             Describe the issue and how to recreate the issue
                         </label>
-                        <textarea
+                        <input
+                            name="description"
                             id="describe"
-                            value={description}
+                            value={formData.description}
                             onChange={(e) => {
-                                setIssueName(e.target.value);
+                                handleChange(e);
                             }}
                             className="w-full p-2 border border-gray-300 dark:border-gray-700 rounded-lg bg-white dark:bg-gray-700 text-gray-900 dark:text-gray-200"
                             required
@@ -91,9 +114,13 @@ const Signup: React.FC = () => {
 
                     <button
                         type="submit"
+                        disabled={loading}
+                        onClick={(e: React.MouseEvent<HTMLButtonElement>) => {
+                            handleSubmit(e);
+                        }}
                         className="w-full bg-blue-500 text-white p-2 rounded-lg hover:bg-blue-600 dark:hover:bg-blue-700 transition duration-200"
                     >
-                        Submit
+                        {loading ? "Loading..." : "Submit"}
                     </button>
                 </form>
             </div>
